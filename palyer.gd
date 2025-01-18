6f44fb22832166296a284fdab90ccec99d8f39d0
YoiDuki <913197494sayonana@gmail.com>
2025-01-18 10:16:20 +0800

    add bubble durability

--- palyer.gd
+++ palyer.gd
@@ -1,17 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -200.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+
+var current_platform = null:
+	set(value):
+		print(current_platform)
+		if current_platform == value:
+			return
+		current_platform = value
+		# when jump on bubble for multiple times, make it boom
+		if current_platform is Bubble:
+			current_platform.durability_decrease()
+			pass
 
 
 func _physics_process(delta: float) -> void:
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		move_and_slide()
-		return
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -26,3 +34,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	var collision_count = get_slide_collision_count()
+	if collision_count == 0:
+		current_platform = null
+		pass
+	for i in collision_count:
+		var collision = get_slide_collision(i)
+		print("Collided with: ", collision.get_collider().name)
+		current_platform = collision.get_collider()
--- static_bubble.gd
+++ static_bubble.gd
@@ -1,24 +1,32 @@
-extends StaticBody2D
+extends AnimatableBody2D
+class_name Bubble
 
-
-const SPEED = 200.0
+const SPEED = 100.0
 
 @onready var sprite: Sprite2D = $Bubble
 @onready var collision: CollisionShape2D = $CollisionShape2D
+
+@export var durability = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sprite.scale = Vector2.ONE * .1
 	collision.scale = Vector2.ONE * .1
+	sync_to_physics = true
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	sprite.scale += Vector2.ONE * .1 * delta
 	collision.scale += Vector2.ONE * .1 * delta
 	position += Vector2.UP * SPEED * delta
 
 
-# Called when the collision shape enters another body.
-func _on_body_entered(body: Node) -> void:
-	print("Collided with: ", body.name)
+func durability_decrease():
+	durability -= 1
+	if durability <= 0:
+		destroy()
+
+
+func destroy():
+	queue_free()
